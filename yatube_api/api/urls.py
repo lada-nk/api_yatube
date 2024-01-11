c87f42e6b279943748fbe82a915f14575f3a0cd5
Лада Колотова <lada.kolotova@gmail.com>
2024-01-11 14:38:35 +0300

    First try

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(
-   r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
 
 
 urlpatterns = [
